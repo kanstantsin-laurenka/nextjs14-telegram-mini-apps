d320f81befc7966b245f32e75c771ac1a3edb0e4
Kanstantsin Laurenka <132393513+koslavr@users.noreply.github.com>
2024-09-07 15:13:18 +0400

    Add @telegram-apps/sdk-react. tsconfig.json "allowJs" changed to flase.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,16 @@
 import { NextUIProvider } from '@nextui-org/react';
+import { SDKProvider } from '@telegram-apps/sdk-react';
 
 export function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <SDKProvider acceptCustomStyles={true} debug={true}>
+        {children}
+      </SDKProvider>
+    </NextUIProvider>
+  );
 }
