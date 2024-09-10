c2ecc8418985490ad1ccaafc0c7c95a0eeb20f88
Kanstantsin Laurenka <132393513+koslavr@users.noreply.github.com>
2024-09-10 20:10:15 +0400

    Configured @telegram-apps/sdk-react and NextUI. Added tonconnect. Updated README.md

---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import React, { type PropsWithChildren, useEffect, useMemo } from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+import {
+  SDKProvider,
+  useLaunchParams,
+  useMiniApp,
+  useThemeParams,
+  useViewport,
+  bindMiniAppCSSVars,
+  bindThemeParamsCSSVars,
+  bindViewportCSSVars,
+} from '@telegram-apps/sdk-react';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { ErrorPage } from '@/components/ErrorPage';
+import { useTelegramMock } from '@/hooks/useTelegramMock';
+import { useDidMount } from '@/hooks/useDidMount';
+
+import styles from './styles.module.css';
+
+function App(props: PropsWithChildren) {
+  const miniApp = useMiniApp();
+  const themeParams = useThemeParams();
+  const viewport = useViewport();
+
+  useEffect(() => {
+    return bindMiniAppCSSVars(miniApp, themeParams);
+  }, [miniApp, themeParams]);
+
+  useEffect(() => {
+    return bindThemeParamsCSSVars(themeParams);
+  }, [themeParams]);
+
+  useEffect(() => {
+    return viewport && bindViewportCSSVars(viewport);
+  }, [viewport]);
+
+  return <>{props.children}</>;
+}
+
+function RootInner({ children }: PropsWithChildren) {
+  // Mock Telegram environment in development mode if needed.
+  if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTelegramMock();
+  }
+
+  const debug = useLaunchParams().startParam === 'debug';
+  const manifestUrl = useMemo(() => {
+    return new URL('tonconnect-manifest.json', window.location.href).toString();
+  }, []);
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    if (debug) {
+      import('eruda').then((lib) => lib.default.init());
+    }
+  }, [debug]);
+
+  return (
+    <NextUIProvider>
+      <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <SDKProvider acceptCustomStyles debug={debug}>
+          <App>{children}</App>
+        </SDKProvider>
+      </TonConnectUIProvider>
+    </NextUIProvider>
+  );
+}
+
+export function Root(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of the Server Side
+  // Rendering. That's why we are showing loader on the server side.
+  const didMount = useDidMount();
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props} />
+    </ErrorBoundary>
+  ) : (
+    <div className={styles.root__loading}>Loading</div>
+  );
+}
